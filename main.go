bf279838a4c768037a0634f96f5046ae91f204de
John Axel Eriksson <john@insane.se>
2018-10-08 13:14:50 +0200

    Improve opacity changing.
    Take the ws layout into account basically.

--- main.go
+++ main.go
@@ -1,23 +1,83 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
 
 	i3 "github.com/johnae/go-i3"
 )
 
+func currentWorkspace() *i3.Node {
+	t, _ := i3.GetTree()
+	focusedWs := t.Root.FindFocused(func(node *i3.Node) bool {
+		if node.Type == i3.WorkspaceNode {
+			n := node.FindFocused(func(node *i3.Node) bool {
+				return node.Focused
+			})
+			return n != nil
+		}
+		return false
+	})
+	return focusedWs
+}
+
+func currentLayout(ws *i3.Node) string {
+	if ws != nil {
+		return string(ws.Layout)
+	}
+	return ""
+}
+
+func updateOpacity(layout string) {
+	fmt.Printf("update opacity: %s\n", layout)
+	if layout == "tabbed" || layout == "stacked" {
+		exec.Command("sway", "[tiling] opacity 1;").CombinedOutput()
+	} else {
+		exec.Command("sway", "[tiling] opacity 0.78; opacity 1").CombinedOutput()
+	}
+}
+
+func resetOpacity() {
+	exec.Command("sway", "[tiling] opacity 1;").CombinedOutput()
+}
+
 func main() {
-	recv := i3.Subscribe(i3.WindowEventType)
-	exec.Command("sway", "[title=\".*\"] opacity 1.0").CombinedOutput()
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signals
+		t, _ := i3.GetTree()
+		if len(t.Root.Nodes) > 1 {
+			resetOpacity()
+		}
+		os.Exit(0)
+	}()
+	recv := i3.Subscribe(i3.WindowEventType, i3.BindingEventType)
+	t, _ := i3.GetTree()
+	// bug in sway which makes it segfault if there are no windows
+	if len(t.Root.Nodes) > 1 {
+		ws := currentWorkspace()
+		resetOpacity()
+		updateOpacity(currentLayout(ws))
+	}
 	for recv.Next() {
-		ev := recv.Event().(*i3.WindowEvent)
-		if ev.Change == "focus" {
-			log.Printf("change: %s\n", ev.Change)
-
-			exec.Command("sway", "[title=\".*\"] opacity 0.78").CombinedOutput()
-
-			exec.Command("sway", "opacity", "1").CombinedOutput()
+		event := recv.Event()
+		if ev, ok := event.(*i3.WindowEvent); ok {
+			if ev.Change == "focus" {
+				ws := currentWorkspace()
+				updateOpacity(currentLayout(ws))
+			}
+		} else if ev, ok := event.(*i3.BindingEvent); ok {
+			cmd := strings.Split(ev.Binding.Command, " ")[0]
+			if cmd == "layout" {
+				ws := currentWorkspace()
+				updateOpacity(currentLayout(ws))
+			}
 		}
 	}
 	log.Fatal(recv.Close())
